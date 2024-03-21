e97b511300117512c576dc68891225af63ce0784
Gurunanthan <gurunanthan.r.j@gmail.com>
2024-03-21 22:14:07 +0530

    added githuub commiting script

---
+++ Intermediate/gitCommitScript.py
@@ -0,0 +1,31 @@
+import os
+
+def print_status():
+    # Display the status of the repository
+    os.system("git status")
+
+def git_commit():
+    # Print the status before committing
+    print("Status before committing:")
+    print_status()
+
+    # Get the commit message from the user
+    commit_message = input("Enter commit message: ")
+
+    # Add all modified files
+    os.system("git add .")
+
+    # Commit changes with the provided commit message
+    os.system('git commit -m "%s"' % commit_message)
+
+    # Prompt the user to confirm the commit
+    confirm_commit = input("Are you sure you want to commit changes? (yes/no): ").lower()
+    if confirm_commit == "yes":
+        # Push changes to the specified branch
+        branch = input("Enter branch (e.g., 'master' or 'main'): ")
+        os.system("git push origin %s" % branch)
+    else:
+        print("Commit canceled.")
+
+if __name__ == "__main__":
+    git_commit()
