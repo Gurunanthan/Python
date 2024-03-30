19db3bae8087563515fd90074c12e5e04a05ccd6
Gurunanthan <gurunanthan.r.j@gmail.com>
2024-03-30 12:42:36 +0530

    changes in file read

--- Basics/fileReadWrite.py
+++ Basics/fileReadWrite.py
@@ -1,5 +1,5 @@
 def fileWrite():
-    with open("Example.txt",'w') as file:
+    with open("Basics/Example.txt",'w') as file:
         content = input("Enter the content for the file")
         file.write(content)
         file.close()
